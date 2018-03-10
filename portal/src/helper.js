6d10e92e8d6f56d914d45d770e7a60b723e9cce5
Michael Ruecker <mich@elruecker.de>
2018-03-10 20:59:28 +0100

    if a store can not be loaded, the rest of the portal will still work properly

--- portal/src/helper.js
+++ portal/src/helper.js
@@ -7,15 +7,23 @@ export function hashPrefix(prefix) {
 }
 
 export async function loadApp(name, hash, appURL, storeURL, globalEventDistributor) {
-    // import the store module
-    const storeModule = storeURL ? await SystemJS.import(storeURL) : {storeInstance: null};
+    let storeModule = {}, customProps = {globalEventDistributor: globalEventDistributor};
 
-    // register the store with the globalEventDistributor
-    if (storeModule.storeInstance && globalEventDistributor)
+    // try to import the store module
+    try {
+        storeModule = storeURL ? await SystemJS.import(storeURL) : {storeInstance: null};
+    } catch (e) {
+        console.log(`Could not load store of app ${name}.`, e);
+    }
+
+    if (storeModule.storeInstance && globalEventDistributor) {
+        // add a reference of the store to the customProps
+        customProps.store = storeModule.storeInstance;
+
+        // register the store with the globalEventDistributor
         globalEventDistributor.registerStore(storeModule.storeInstance);
+    }
 
     // register the app with singleSPA and pass a reference to the store of the app as well as a reference to the globalEventDistributor
-    const customProps = { store: storeModule.storeInstance, globalEventDistributor: globalEventDistributor };
-
     singleSpa.registerApplication(name, () => SystemJS.import(appURL), hashPrefix(hash), customProps);
 }
